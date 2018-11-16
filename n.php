0e8350400a3e25f2c733c59ca0a0ec922c3900e1
Bergn97 <bergn97@gmail.com>
2018-11-15 19:05:43 -0600

    Add files via upload
    Here are some game phps that work kinda.

---
+++ n.php
@@ -0,0 +1,4 @@
+<body>
+<embed width="792" height="600" base="https://external.kongregate-games.com/gamez/0002/4483/live/" src="https://external.kongregate-games.com/gamez/0002/4483/live/embeddable_24483.swf" type="application/x-shockwave-flash"></embed><br/>Play free games at <a href="https://www.kongregate.com/">Kongregate</a>
+</body>
+
---
+++ spankthemonkey.php
@@ -0,0 +1,4 @@
+
+<body>
+<iframe src="http://www.freeonlinegames.com/embed/204" width="850" height="400" frameborder="no" scrolling="no"></iframe>
+</body>
